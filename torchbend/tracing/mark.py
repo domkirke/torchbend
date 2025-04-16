f72c3980788c72b4e81dfcad0b9f8ede2f57adf8
domkirke <axelchemla@yahoo.fr>
2025-04-16 12:16:59 +0200

    updated mark (still not working when tracing)

--- torchbend/tracing/mark.py
+++ torchbend/tracing/mark.py
@@ -1,4 +1,6 @@
 import functools
+from typing import Any, Optional
+import torch
 import torch.fx as fx
 import torch.nn as nn
 from .proxy import BendingProxy
@@ -7,16 +9,20 @@ def register_alias(obj, name, mode):
     obj.tracer.register_alias(obj.node, name, mode)
     return obj
 
-def mark(*args, obj=None, name=None, mode="post"):
-    if len(args) == 1:
-        assert obj is None
-        obj = args[0]
-    if len(args) == 2: 
-        assert obj is None and name is None
-        obj, name = args
-    if len(args) == 3:
-        obj, name, mode = args
-    if len(args) > 3: raise RuntimeError('mark must be given obj, or obj+name')
+def mark_decorator(fn):
+    if callable(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            out = fn(*args, **kwargs)
+            out = mark(obj=out, name=name)
+            return out
+        return wrapper
+    else:
+        raise RuntimeError()
+
+
+@torch.jit.ignore
+def mark(obj: Optional[Any] = None, name: Optional[str] = None, mode: Optional[str] = "post") -> Any:
     if obj is not None:
         if isinstance(obj, BendingProxy):
             assert obj is not None
@@ -28,15 +34,4 @@ def mark(*args, obj=None, name=None, mode="post"):
             return obj
     else: 
         # used as a decorator
-        def mark_decorator(fn):
-            if callable(fn):
-                @functools.wraps(fn)
-                def wrapper(*args, **kwargs):
-                    out = fn(*args, **kwargs)
-                    out = mark(obj=out, name=name)
-                    return out
-                return wrapper
-            else:
-                raise RuntimeError()
-
         return mark_decorator
